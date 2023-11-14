434f207f87850715d029a8016b8077de39afd9a4
yassinex02 <tahayassinemoumni@gmail.com>
2023-11-14 22:07:37 +0100

    old v

--- app.py
+++ app.py
@@ -1,78 +1,97 @@
-from flask import Flask, render_template_string, request, render_template
-from flask_wtf.csrf import CSRFProtect
-from flask_talisman import Talisman
-import os
+from flask import Flask, render_template, request, redirect, url_for
 import pymssql
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 
-# CSRF Protection
-csrf = CSRFProtect(app)
-
-# Content Security Policy and Clickjacking Protection
-Talisman(app, content_security_policy=None, frame_options='SAMEORIGIN')
-
-# Database connection
+# Function to establish a database connection
 
 
 def get_db_connection():
     username = "yassine"
     password = os.environ.get("AZURE_SQL_PASSWORD")
-    server = "asesql2.database.windows.net"
-
+    server = "asesql2.database.windows.net"  # change to your server
     database = "ase_sql"
-
     return pymssql.connect(server, username, password, database)
 
-
-def is_zap_request():
-    # Check if the request is likely coming from the ZAP scanner
-    user_agent = request.headers.get('User-Agent')
-    return 'ZAP' in user_agent
+# Function to create the user_info table if it doesn't exist
 
 
-@app.route('/')
-def index():
-    return render_template_string(open('templates/index.html').read())
+def create_table_if_not_exists(conn):
+    cursor = conn.cursor()
+    cursor.execute('''
+        IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='user_info' and xtype='U')
+        CREATE TABLE user_info (
+            id INT IDENTITY(1,1) PRIMARY KEY,
+            name VARCHAR(255),
+            age INT
+        )
+    ''')
+    conn.commit()
+    cursor.close()
+
+# Function to insert data into the user_info table
 
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    # Check if the request is likely coming from the ZAP scanner
-    if is_zap_request():
-        print("Ignoring request from ZAP scanner.")
-        return render_template_string('<h1>Request Ignored</h1>')
-
-    name = request.form['name']
-    age = request.form['age']
-    print(f"Received name: {name}, age: {age}")
-
-    # Insert into database
-    conn = get_db_connection()
+def insert_data(conn, name, age):
     cursor = conn.cursor()
     cursor.execute(
         "INSERT INTO user_info (name, age) VALUES (%s, %s)", (name, age))
     conn.commit()
     cursor.close()
-    conn.close()
 
-    return render_template_string(f'''
-            <h1>Hello {name}, you are {age} years old!</h1>
-            <button onclick="window.location.href='/users'">Show All Users</button>
-        ''')
+# Function to query and fetch all data from the user_info table
 
 
-@app.route('/users')
-def users():
-    conn = get_db_connection()
+def query_all_data(conn):
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM user_info")
-    users = cursor.fetchall()
+    data = cursor.fetchall()
     cursor.close()
+    return data
+
+# Route for the home page
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Route to handle form submission
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    # Check if the request is coming from the ZAP scanner
+    if 'OWASP-ZAP-Scan' in request.headers.get('User-Agent', ''):
+        print("Ignoring request from ZAP scanner.")
+        return redirect(url_for('home'))
+
+    name = request.form['name']
+    age = request.form['age']
+
+    # Insert data into the database
+    conn = get_db_connection()
+    create_table_if_not_exists(conn)
+    insert_data(conn, name, age)
     conn.close()
 
-    return render_template('data.html', users=users)
+    return redirect(url_for('home'))
+
+# Route for displaying all data
+
+
+@app.route('/show_database')
+def show_database():
+    # Fetch all data from the database
+    conn = get_db_connection()
+    data = query_all_data(conn)
+    conn.close()
+
+    return render_template('show_database.html', data=data)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
