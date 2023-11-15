cef854a570ede4a36d02ac2400e251074f97f79f
yassinex02 <tahayassinemoumni@gmail.com>
2023-11-15 02:21:35 +0100

    please

--- app.py
+++ app.py
@@ -11,13 +11,19 @@ app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY', 'different_key')
 csrf = CSRFProtect(app)
 
 # Content Security Policy and Clickjacking Protection
-Talisman(app, content_security_policy={
-    'default-src': "'self'",
-    'style-src': "'self' 'unsafe-inline'",
-    'script-src': "'self' 'unsafe-inline'",
-    'img-src': "'self'",
-    'font-src': "'self'",
-}, frame_options='SAMEORIGIN', strict_transport_security=True)
+Talisman(
+    app,
+    content_security_policy={
+        'default-src': "'self'",
+        'style-src': "'self' 'unsafe-inline'",
+        'script-src': "'self' 'unsafe-inline'",
+        'img-src': "'self'",
+        'font-src': "'self'",
+        'frame-ancestors': "'none'"
+    },
+    frame_options='SAMEORIGIN',
+    strict_transport_security=True
+)
 
 # Database connection
 
@@ -44,28 +50,37 @@ def index():
     return render_template_string(open('templates/index.html').read())
 
 
+# ...
+
 @app.route('/submit', methods=['POST'])
 def submit():
-    name = request.form['name']
-    age = request.form['age']
-    print(f"Received name: {name}, age: {age}")
+    try:
+        name = request.form['name']
+        age = request.form['age']
+        print(f"Received name: {name}, age: {age}")
 
-    # Insert into database
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute(
-        "INSERT INTO user_info (name, age) VALUES (%s, %s)", (name, age))
-    conn.commit()
-    cursor.close()
-    conn.close()
+        # Insert into database
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute(
+            "INSERT INTO user_info (name, age) VALUES (%s, %s)", (name, age))
+        conn.commit()
+        cursor.close()
+        conn.close()
 
-    # Show confirmation message
-    confirmation_message = f'Thank you, {name}! Your information has been submitted successfully.'
+        # Show confirmation message
+        confirmation_message = f'Thank you, {name}! Your information has been submitted successfully.'
 
-    return render_template_string(f'''
-        <h1>{confirmation_message}</h1>
-        <button onclick="window.location.href='/view-users'">View All Users</button>
-    ''')
+        return render_template_string(f'''
+            <h1>{confirmation_message}</h1>
+            <button onclick="window.location.href='/view-users'">View All Users</button>
+        ''')
+    except Exception as e:
+        print(f"Error during submission: {e}")
+        error_message = 'An error occurred during submission. Please try again.'
+        return render_template_string(f'<h1>{error_message}</h1>'), 500
+
+# ...
 
 
 @app.route('/view-users')
