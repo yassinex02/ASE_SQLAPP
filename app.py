27f0e230a16e299504a87fc08819476225e156f1
yassinex02 <tahayassinemoumni@gmail.com>
2023-11-15 01:53:20 +0100

    mm

--- app.py
+++ app.py
@@ -11,7 +11,13 @@ app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY', 'default_key')
 csrf = CSRFProtect(app)
 
 # Content Security Policy and Clickjacking Protection
-Talisman(app, content_security_policy=None, frame_options='SAMEORIGIN')
+Talisman(app, content_security_policy={
+    'default-src': "'self'",
+    'style-src': "'self' 'unsafe-inline'",
+    'script-src': "'self' 'unsafe-inline'",
+    'img-src': "'self'",
+    'font-src': "'self'",
+}, frame_options='SAMEORIGIN', strict_transport_security=True)
 
 # Database connection
 
@@ -21,11 +27,16 @@ def get_db_connection():
     password = os.environ.get("AZURE_SQL_PASSWORD")
     server = "asesql2.database.windows.net"
     database = "ase_sql"
-
-    if password is None:
-        raise ValueError("Database password is not provided.")
-
     return pymssql.connect(server, username, password, database)
+
+# Error handling
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template_string('<h1>Internal Server Error</h1>'), 500
+
+# Routes
 
 
 @app.route('/')
@@ -35,39 +46,35 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    try:
-        name = request.form['name']
-        age = request.form['age']
-        print(f"Received name: {name}, age: {age}")
+    name = request.form['name']
+    age = request.form['age']
+    print(f"Received name: {name}, age: {age}")
 
-        # Insert into database
-        with get_db_connection() as conn:
-            with conn.cursor() as cursor:
-                cursor.execute(
-                    "INSERT INTO user_info (name, age) VALUES (%s, %s)", (name, age))
-            conn.commit()
+    # Insert into database
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO user_info (name, age) VALUES (%s, %s)", (name, age))
+    conn.commit()
+    cursor.close()
+    conn.close()
 
-        return render_template_string(f'''
-                <h1>Hello {name}, you are {age} years old!</h1>
-                <button onclick="window.location.href='/users'">Show All Users</button>
-            ''')
-
-    except Exception as e:
-        return f"Error: {str(e)}", 500
+    return render_template_string(f'''
+        <h1>Hello {name}, you are {age} years old!</h1>
+        <button onclick="window.location.href='/users'">Show All Users</button>
+    ''')
 
 
 @app.route('/users')
 def users():
-    try:
-        with get_db_connection() as conn:
-            with conn.cursor() as cursor:
-                cursor.execute("SELECT * FROM user_info")
-                users = cursor.fetchall()
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM user_info")
+    users = cursor.fetchall()
+    cursor.close()
+    conn.close()
 
-        return render_template('data.html', users=users)
-
-    except Exception as e:
-        return f"Error: {str(e)}", 500
+    return render_template('data.html', users=users)
 
 
 if __name__ == '__main__':
